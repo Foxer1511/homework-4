dad0414b933dc99f7493be3dcc8c5e596f045988
Yurii Vengry <foxyura13@gmail.com>
2023-07-13 23:34:28 +0300

    1 task complete

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+# # 1. Користувач вводить рядок з клавіатури.
+# # Порахуйте кількість літер, цифр у рядку.
+# # Виведіть обидві кількості на екран. (використовувати цикл)
+# line = input("Write some symbols and digits ")
+# ii = 0
+# dd = 0
+# for i in line:
+#     if i.isalpha():
+#         ii += 1
+#
+# print("words", ii)
+# for d in line:
+#     if d.isdigit():
+#         dd += 1
+# print("digits", dd)
 
 
 

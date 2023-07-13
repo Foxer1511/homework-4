912f511e35d761f95498cb4b79ec6ac081a75a1b
Yurii Vengry <foxyura13@gmail.com>
2023-07-13 23:52:24 +0300

    2 task complete

--- main.py
+++ main.py
@@ -14,5 +14,16 @@
 #         dd += 1
 # print("digits", dd)
 
+# 2. Користувач вводить з клавіатури рядок та символ для пошуку.
+# Порахуйте скільки разів у рядку зустрічається потрібний символ.
+# Отримане число виведіть на екран.
+
+# line = input("Enter some symbols ")
+# search = input("Enter symbol what you need to find ")
+# id = 0
+# for i in line:
+#     if i == search:
+#         id += 1
+# print("symbols ", id)
 
 
